19814ed1a94f5b77fce9548fccabf8f05b64f0ad
ngnigel99 <e0543444@u.nus.edu>
2021-09-03 17:55:02 +0800

    Add lecture changes, comments as what prof covered

---
+++ src/main/java/circus/Trainer.java
@@ -0,0 +1,41 @@
+public class Trainer {
+    public static void main(String[] args) {
+        Duck d = new Duck();
+        getToSpeak(d);
+
+        Bird b = (Bird) d;  // upcasting
+        getToSpeak(b);
+
+        /*
+         * Note the redundancy check by compiler --> user no needs to worry!
+         */
+        Animal a = (Animal) b; // upcasting
+        getToSpeak(a);
+
+        Duck d2 = (Duck) a; // downcasting
+        getToSpeak(d2);
+
+        train(new Duck());
+        train(new Parrot());
+
+        //removed to reflect lack of instantiation
+        //prevent generic object, attach to something
+        //Animal a2 = new Animal();
+        //Bird b2 = new Bird();
+
+    }
+
+    private static void getToSpeak(Animal animal) {
+        System.out.println(animal.speak());
+    }
+
+    private static void train(Bird bird) {
+        //modification 1: use underlying object
+        if (bird instanceof Duck) {
+            Duck d = (Duck) bird;
+            d.swim();
+        } else {
+            System.out.println("Not a duck! Not a duck!");
+        }
+    }
+}
---
+++ src/main/java/circus/animal/Animal.java
@@ -0,0 +1,11 @@
+package circus;
+
+public abstract class Animal implements Asset {
+
+    //delete method body for abstract method
+    public abstract String speak();
+
+        public int getValue() {
+            return 10;
+        }
+    };
